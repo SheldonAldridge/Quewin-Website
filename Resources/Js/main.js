e2af0ed8738403cc2247995db74eda8e7231b0bd
Sheldon <sheldonaldridge@yahoo.com>
2024-05-12 17:16:48 +0200

    Create Vue Nav Object

--- Resources/Js/main.js
+++ Resources/Js/main.js
@@ -1,9 +1,15 @@
-const app = Vue.createApp({
-   date(){
-    return{
-        
-    }
+const navbar = Vue.createApp({
+   data(){
+      return{
+         objectNav:{
+          burgerMen:'./Resources/Images/Navbar/Burger-Menu.svg',
+          burgerMenSlide:'./Resources/Images/Navbar/Burger-Menu-Slide.svg',
+          home:`Home`,
+          aboutMe: `About Me`,
+          mywork:`My Work`
+         }
+      }
    }
 });
 
-app.mount('#website')
+navbar.mount('#navbar')
