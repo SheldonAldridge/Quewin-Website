4787a6df62b5c45f0b0b1bf6cfd70d566b37c79e
Sheldon <sheldonaldridge@yahoo.com>
2024-05-16 19:26:55 +0200

    Update

--- Resources/Js/main.js
+++ Resources/Js/main.js
@@ -1,68 +1,8 @@
-const burgNavbar = Vue.createApp({
-   data(){
-      return{
-         objectNav: {
-          burgerMen:'./Resources/Images/Navbar/Burger-Menu.svg',
-          burgerMenSlide:'./Resources/Images/Navbar/Burger-Menu-Slide.svg',
-          home:`Home`,
-          aboutMe: `About Me`,
-          mywork:`My Work`
-         },
-
-         objectLink: {
-            homeImage: './Resources/Images/Images - artwork/Home.png',
-            aboutImage: './Resources/Images/Images - artwork/AboutMe',
-            myWorkImage: './Resources/Images/Images - artwork/MyWork',
-         },
-
-         //Toggle Burger Menu Images
-         ShowBurgerMen: true,
-         ShowBurgerMenSlide: false,
-         ShowMenuList: false,
-
-         //Toggle padding
-         BurgPadding: false,
-
-         //burger Slide Menu animation
-         slideIn: true,
-
-         //Menu list slide down animation
-         slideDown: true,
-
-      };
-   },
-      methods:{
-         toggleBurger() {
-            this.ShowBurgerMen = !this.ShowBurgerMen,
-            this.ShowBurgerMenSlide = !this.ShowBurgerMenSlide,
-            this.ShowMenuList = !this.ShowMenuList
-            this.BurgPadding = !this.BurgPadding
-            
-         },
-
-         toggleBurgerSlide() {
-            this.ShowBurgerMenSlide = !this.ShowBurgerMenSlide,
-            this.ShowBurgerMen = !this.ShowBurgerMen,
-            this.ShowMenuList = !this.ShowMenuList
-         },
-
-         toggleSlideIn(){
-            this.slideIn = !this.slideIn
-         },
-         toggleSlideDown(){
-            this.slideDown = !this.slideDown
-         },
-      },
-
-});
-
-burgNavbar.mount('#burger-nav')
-
 const nav = Vue.createApp({
    data(){
       return{
          listNavObj: {
-            portfolio: 'Portfolio',
+            home: 'Portfolio',
             aboutMe: 'About Me',
             ContactMe: 'Contact Me'
          },
--- Resources/Js/portfolio.js
+++ Resources/Js/portfolio.js
@@ -1,27 +1,106 @@
-const nameHeader = Vue.createApp({
+const burgNavbar = Vue.createApp({
     data(){
        return{
-          nameTitleObj: {
-             firstName: 'Quewin',
-             lastName: 'Fortune'
+          objectNav: {
+           burgerMen:'./Resources/Images/Navbar/Burger-Menu.svg',
+           burgerMenSlide:'./Resources/Images/Navbar/Burger-Menu-Slide.svg',
           },
 
+          listNavObj: {
+            home: 'Home',
+            aboutMe: 'About Me',
+            ContactMe: 'Contact Me'
+         },
+ 
           projectPage:{
             title: 'Portfolio'
-          }
-       }
-    }
-});
+          },
 
-nameHeader.mount('#nameHeader');
+          projectTitleObj:{
+            digitalArt: 'Digital Art Work',
+            traditionalArt: 'Traditional Art Work',
+            animation: 'Animation Work'
+        },
+ 
+          //Toggle Burger Menu Images
+          ShowBurgerMen: true,
+          ShowBurgerMenSlide: false,
+          ShowMenuList: false,
+ 
+          //Toggle padding
+          BurgPadding: false,
+ 
+          //burger Slide Menu animation
+          slideIn: true,
+ 
+          //Menu list slide down animation
+          slideDown: true,
+ 
+       };
+    },
+       methods:{
+          toggleBurger() {
+             this.ShowBurgerMen = !this.ShowBurgerMen,
+             this.ShowBurgerMenSlide = !this.ShowBurgerMenSlide,
+             this.ShowMenuList = !this.ShowMenuList
+             this.BurgPadding = !this.BurgPadding
+             
+          },
+ 
+          toggleBurgerSlide() {
+             this.ShowBurgerMenSlide = !this.ShowBurgerMenSlide,
+             this.ShowBurgerMen = !this.ShowBurgerMen,
+             this.ShowMenuList = !this.ShowMenuList
+          },
+ 
+          toggleSlideIn(){
+             this.slideIn = !this.slideIn
+          },
+          toggleSlideDown(){
+             this.slideDown = !this.slideDown
+          },
+       },
+ 
+ });
+
+ burgNavbar.mount('#burger-nav');
 
 const portfolio = Vue.createApp({
     data(){
         return{
-            projectTitleObj:{
-                projectTitle: 'Digital Art Work'
-            }
-        }
+           imageGroups:[
+            [
+                {src: './Resources/Images/Gallery/Digital Art/Image 1.jpg', alt: 'Image 1'}
+            ],
+
+            [
+                {src: './Resources/Images/Gallery/Digital Art/Image 3.jpg', alt: 'Image 3'},
+                {src: './Resources/Images/Gallery/Digital Art/Image 2.jpg', alt: 'Image 2'},
+                {src: './Resources/Images/Gallery/Digital Art/Image 4.jpg', alt: 'Image 4'}
+            ],
+
+            [
+                {src: './Resources/Images/Gallery/Digital Art/Image 5.jpg', alt: 'Image 5'}
+            ],
+
+            [
+                {src: './Resources/Images/Gallery/Digital Art/Image 6.jpg', alt: 'Image 6'},
+                {src: './Resources/Images/Gallery/Digital Art/Image 7.jpg', alt: 'Image 7'},
+                {src: './Resources/Images/Gallery/Digital Art/Image 8.jpg', alt: 'Image 8'}
+            ],
+
+            [
+                {src: './Resources/Images/Gallery/Digital Art/Image 9.jpg', alt: 'Image 9'}
+            ],
+
+            [
+                {src: './Resources/Images/Gallery/Digital Art/Image 10.jpg', alt: 'Image 10'},
+                {src: './Resources/Images/Gallery/Digital Art/Image 11.jpg', alt: 'Image 11'},
+                {src: './Resources/Images/Gallery/Digital Art/Image 16.jpg', alt: 'Image 16'}
+            ],
+
+           ]
+        };
     }
 })
 
