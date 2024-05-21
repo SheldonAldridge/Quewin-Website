4e955b3fe8fa3a73d40a48c9346aab7557233809
Sheldon <sheldonaldridge@yahoo.com>
2024-05-21 13:22:40 +0200

    Update

--- Resources/Js/main.js
+++ Resources/Js/main.js
@@ -12,7 +12,57 @@ const nav = Vue.createApp({
             lastName: 'Fortune'
          }
       }
-   }
+   },
+
+    //Toggle Burger Menu Images
+    ShowBurgerMen: true,
+    ShowBurgerMenSlide: false,
+    ShowMenuList: false,
+
+    //Toggle padding
+    BurgPadding: false,
+
+    //burger Slide Menu animation
+    slideIn: true,
+
+    //Menu list slide down animation
+    slideDown: true,
+
+    ShowDigArt: true,
+
+    methods:{
+      toggleBurger() {
+         this.ShowBurgerMen = !this.ShowBurgerMen,
+         this.ShowBurgerMenSlide = !this.ShowBurgerMenSlide,
+         this.ShowMenuList = !this.ShowMenuList,
+         this.BurgPadding = !this.BurgPadding
+      },
+
+      toggleBurgerSlide() {
+         this.ShowBurgerMenSlide = !this.ShowBurgerMenSlide,
+         this.ShowBurgerMen = !this.ShowBurgerMen,
+         this.ShowMenuList = !this.ShowMenuList
+         
+      },
+
+      toggleSlideIn(){
+         this.slideIn = !this.slideIn
+      },
+      toggleSlideDown(){
+         this.slideDown = !this.slideDown
+      },
+
+      toggleDigArt() {
+        this.ShowDigArt = true,
+        this.ShowTradArt = false
+      },
+
+      toggleTradArt() {
+        this.ShowDigArt = false,
+        this.ShowTradArt = true
+      },
+
+   },
 });
 
 nav.mount('#nav');
