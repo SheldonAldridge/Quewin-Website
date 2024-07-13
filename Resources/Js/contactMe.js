23a3aeb70666618118caa3b72bfce3a3b54cc30a
Sheldon <sheldonaldridge@yahoo.com>
2024-07-13 18:59:58 +0200

    Update

--- Resources/Js/contactMe.js
+++ Resources/Js/contactMe.js
@@ -1,18 +1,3 @@
-const heroImage = Vue.createApp({
-    data() {
-      return {
-        heroImage: {
-          QuweinImage:"./Resources/Images/Quewin Images/69543088-01f5-4102-9bde-d2420736d14d.jpg",
-        },
-
-        title:{
-            heroTitle:'Contact Me',
-        }
-      };
-    },
-  });
-  
-  heroImage.mount('#hero-image')
 
   const title = Vue.createApp({
     data() {
@@ -40,15 +25,37 @@ const heroImage = Vue.createApp({
   const media = Vue.createApp({
     data() {
       return {
-  
         socialMedia:{
           facebook:"/Resources/Images/Contact_Page_Images/facebook.png",
-          linkedIn:"/Resources/Images/Contact_Page_Images/linkedin.png",
-          pinterest:"/Resources/Images/Contact_Page_Images/Pinterest.png",
-          youtube:"/Resources/Images/Contact_Page_Images/youtube.png"
+          facebookLink:"https://m.facebook.com/quewin.fortuin/",
+          tapas:"/Resources/Images/Contact_Page_Images/tapas.png",
+          tapasLink:"https://m.tapas.io/quewinty777",
+          instagram:"/Resources/Images/Contact_Page_Images/Instagram.png",
+          instagramLink:"https://www.instagram.com/wolfyque_art777/",
+          youtube:"/Resources/Images/Contact_Page_Images/youtube.png",
+          youtubeLink:"https://www.youtube.com/playlist?list=PLZIo7ASKMTki7VlMYXtF-24dgoCDQy14M",
+          tickTock:"/Resources/Images/Contact_Page_Images/TickTok.png",
+          tickTockLink:"https://www.tiktok.com/@wqa777"
         }
       };
     },
   });
 
   media.mount('#media')
+
+  const contactinfo = Vue.createApp({
+    data() {
+      return {
+
+        contactMe:{
+          email:"/Resources/Images/Contact_Page_Images/Email_address.png",
+          emailText:"quewinty777@gmail.com",
+          phone:"/Resources/Images/Contact_Page_Images/smartphone_contact_info.png",
+          phoneText:"(+27) 81 361 0615"
+        }
+      };
+    },
+  });
+  
+  contactinfo.mount('#contactInfo')
+  
